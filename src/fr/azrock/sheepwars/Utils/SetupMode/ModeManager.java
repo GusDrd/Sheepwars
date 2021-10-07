f7a5a695cafb5996bcca77824cfea3605b8757d0
GusDrd <dirandaugustin@gmail.com>
2021-10-07 01:23:22 +0100

    Created SetupMode classes to remember for later

---
+++ src/fr/azrock/sheepwars/Utils/SetupMode/ModeManager.java
@@ -0,0 +1,5 @@
+package fr.azrock.sheepwars.Utils.SetupMode;
+
+public class ModeManager {
+
+}
---
+++ src/fr/azrock/sheepwars/Utils/SetupMode/SetupMode.java
@@ -0,0 +1,11 @@
+package fr.azrock.sheepwars.Utils.SetupMode;
+
+public enum SetupMode {
+
+	LOBBY,
+	SPEC_LOBBY,
+	BLUE_SPAWNS,
+	RED_SPAWNS,
+	BONUS;
+	
+}
